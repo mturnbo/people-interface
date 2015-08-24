0ec686a46eba3b1bae0afc06c5e4251b405e3f02
Marcus E. Turnbo <marcus@emarcus.com>
2015-08-24 02:20:05 -0500

    Display list of person and family.  Added for for new person and family.

--- js/people.js
+++ js/people.js
@@ -3,7 +3,7 @@ function getPeople() {
         type: 'GET',
         url: '/people-api/person',
         success: function(data) {
-	        $('#peopleTable tbody > tr').remove();
+	        $('#personTable tbody > tr').remove();
             var people = data._embedded.person;
             $.each(people, function(index) {
 	            $("#personTable tbody").append('<tr><td>' + people[index].firstName + '</td><td>' + people[index].lastName + '</td><td>' + people[index].birthDate + '</td><</tr>');
@@ -26,7 +26,66 @@ function getFamilies() {
     });	
 }
 
+function addPerson(newPerson) {
+	$.ajax({
+		type: 'POST',
+		contentType: 'application/json',
+		url: '/people-api/person',
+		dataType: 'json',
+		data: newPerson,
+		statusCode: {
+			201: function() {
+				getPeople();
+			}
+    	}
+	});
+}
+
+function addFamily(newFamily) {
+	$.ajax({
+		type: 'POST',
+		contentType: 'application/json',
+		url: '/people-api/family',
+		dataType: 'json',
+		data: newFamily,
+		success: function() {
+			getFamilies();
+		}
+	});
+}
+
+$.fn.serializeObject = function () {
+	var o = {};
+	var a = this.serializeArray();
+	$.each(a, function () {
+		if (o[this.name] !== undefined) {
+			if (!o[this.name].push) {
+				o[this.name] = [o[this.name]];
+			}
+			o[this.name].push(this.value || '');
+		} else {
+			o[this.name] = this.value || '';
+		}
+	});
+	return o;
+ };
+
 $(document).ready(function() {
+	
+	$('#frmNewPerson').submit(function() {
+		var data = JSON.stringify($(this).serializeObject());
+		addPerson(data);
+		$(this).trigger('reset');
+		return false;
+	});
+	
+	$('#frmNewFamily').submit(function() {
+		var data = JSON.stringify($(this).serializeObject());
+		addFamily(data);
+		$(this).trigger('reset');
+		return false;
+	});
+	
 	getPeople();
 	getFamilies();
 });
